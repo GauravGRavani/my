c56bf4f5b7909610b34740c0d225ae292222e064
GauravGRavani <68845412+GauravGRavani@users.noreply.github.com>
2022-04-03 18:19:14 +0530

    update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,16 +1,26 @@
-import requests
-def get_tor_session():
-    session = requests.session()
-    # Tor uses the 9050 port as the default socks port
-    session.proxies = {'http':  'socks5://127.0.0.1:9050',
-                       'https': 'socks5://127.0.0.1:9050'}
-    return session
+import time
+import socket
+import socks
 
-# Make a request through the Tor connection
-# IP visible through Tor
-session = get_tor_session()
-print('tor ip=' + session.get("http://httpbin.org/ip").text)
-# Above should print an IP different than your public IP
+from urllib.request import urlopen
+from stem import Signal
+from stem.control import Controller
 
-# Following prints your normal public IP
-print('my ip ='+requests.get("http://httpbin.org/ip").text)
+controller = Controller.from_port(port=9051)
+
+def connectTor():
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+    socket.socket = socks.socksocket
+
+def renewTor():
+    controller.authenticate("1")
+    controller.signal(Signal.NEWNYM)
+
+def showIP():
+    print(urlopen('http://icanhazip.com').read())
+
+for i in range(5):
+    renewTor()
+    connectTor()
+    showIP()
+    time.sleep(10)
