0644e2a6e29d177417a775d7591ff74b84df6aa5
GauravGRavani <68845412+GauravGRavani@users.noreply.github.com>
2022-04-03 16:49:08 +0530

    update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,4 +1,16 @@
 import requests
-get = requests.get('https://httpbin.org/ip')
-get.text
-print(get.text)
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
+
+# Make a request through the Tor connection
+# IP visible through Tor
+session = get_tor_session()
+print('tor ip=' + session.get("http://httpbin.org/ip").text)
+# Above should print an IP different than your public IP
+
+# Following prints your normal public IP
+print('my ip ='+requests.get("http://httpbin.org/ip").text)
