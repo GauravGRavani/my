765c1aaec263722c4c974b8e5dd8df1d71c5f460
GauravGRavani <68845412+GauravGRavani@users.noreply.github.com>
2022-04-04 21:08:40 +0530

    update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,5 +1,7 @@
 from selenium import webdriver  
+import time
  driver = webdriver.Firefox(executable_path=r'home/kali/Downloads)
 driver.get('https://www.lifeofpentester.blogspot.com')
+time.sleep(4)
 # insert time.sleep() here  
 driver.close()
