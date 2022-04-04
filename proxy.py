bd2767c02068634087650eb63afd34e2a178bce7
GauravGRavani <68845412+GauravGRavani@users.noreply.github.com>
2022-04-04 21:07:50 +0530

    update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,7 +1,5 @@
 from selenium import webdriver  
-from selenium.webdriver.firefox.firefox\_binary import FirefoxBinary  
-binary = FirefoxBinary('/root/Downloads/firefox/firefox')  
-driver = webdriver.Firefox(firefox\_binary=binary)  
+ driver = webdriver.Firefox(executable_path=r'home/kali/Downloads)
 driver.get('https://www.lifeofpentester.blogspot.com')
 # insert time.sleep() here  
 driver.close()
